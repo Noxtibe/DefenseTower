323c89120a30650b75fd746c354114c101824b74
Benoit <bbarrau@artfx.fr>
2022-11-03 16:23:16 +0100

    Turret + Waves
    Turrets can now follow enemies.
    Waves of enemies increase with waves.

---
+++ Assets/Script/SC_Turret.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Turret : MonoBehaviour
+{
+    [Header("Turret options")]
+    [SerializeField] public Transform target;
+    [SerializeField] public Transform partToRotate;
+    [SerializeField] public float range = 15f;
+    [SerializeField] public string enemyTag = "Enemy";
+    [SerializeField] private float turnSpeed = 6f;
+
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] ennemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach(GameObject enemy in ennemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    void Update()
+    {
+        if(target == null)
+        {
+            return;
+        }
+
+        Vector3 dir  = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
--- Assets/Script/SC_WaveSpawner.cs
+++ Assets/Script/SC_WaveSpawner.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
 
 public class SC_WaveSpawner : MonoBehaviour
 {
     [Header("Waves options")]
     [SerializeField] private Transform enemyPrefab;
     [SerializeField] private Transform spawnPoint;
+    [SerializeField] private TMP_Text waveTimerText;
+    [Tooltip("Always make sure the time betweens wave is rounded at 0.5")]
+    [TextArea] public string waveTextTip;
+    //[InspectorTextArea] public string waveTip2;
     [SerializeField] private float timeBetweenWaves = 5f;
     [SerializeField] private float timer = 2f;
     [SerializeField] private int waveIndex = 0;
@@ -21,6 +28,7 @@ public class SC_WaveSpawner : MonoBehaviour
         }
 
         timer -= Time.deltaTime;
+        waveTimerText.text = Mathf.Round(timer).ToString(); // Round better to Floor, cause 30 fps unity, too fast for this
     }
 
     IEnumerator SpawnWave()
