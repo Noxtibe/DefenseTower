eea09b9fd61b7a43876799e21d6d9d03e31083a4
Benoit <bbarrau@artfx.fr>
2022-11-04 10:25:59 +0100

    Fire
    Turret can now fire and destroy enemies with a little impact

---
+++ Assets/Script/SC_Bullet.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Bullet : MonoBehaviour
+{
+    [SerializeField] public float bulletSpeed = 50f;
+    [SerializeField] public GameObject fireEffect;
+    private Transform target;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+    void Update()
+    {
+        if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = bulletSpeed * Time.deltaTime;
+        if(dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+    }
+
+    void HitTarget()
+    {
+        Debug.Log("Méchant toucher");
+        GameObject fireEffectInst = (GameObject)Instantiate(fireEffect, transform.position, transform.rotation);
+        Destroy(fireEffectInst, 2f);
+        Destroy(target.gameObject);
+        Destroy(gameObject);
+    }
+}
--- Assets/Script/SC_Turret.cs
+++ Assets/Script/SC_Turret.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class SC_Turret : MonoBehaviour
 {
     [Header("Turret options")]
+    [SerializeField] public GameObject bulletPrefab;
     [SerializeField] public Transform target;
     [SerializeField] public Transform partToRotate;
-    [SerializeField] public float range = 15f;
+    [SerializeField] public Transform firePoint;
     [SerializeField] public string enemyTag = "Enemy";
+    [SerializeField] public float range = 15f;
     [SerializeField] private float turnSpeed = 6f;
+    [SerializeField] public float fireRate = 1f;
+    [SerializeField] private float fireTimer = 0f;
 
     void Start()
     {
@@ -53,6 +57,25 @@ public class SC_Turret : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(fireTimer <= 0f)
+        {
+            Fire();
+            fireTimer = 1 / fireRate;
+        }
+        fireTimer -= Time.deltaTime;
+    }
+
+    void Fire()
+    {
+        Debug.Log("Tir sur méchant");
+        GameObject bulletOB = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        SC_Bullet bullet = bulletOB.GetComponent<SC_Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
     }
 
     private void OnDrawGizmosSelected()
