9de94a1db08c1ef33f26a1e2f435e24d2c784a6c
Benoit <bbarrau@artfx.fr>
2022-11-06 22:59:36 +0100

    Build

--- Assets/Script/SC_Node.cs
+++ Assets/Script/SC_Node.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
 
 public class SC_Node : MonoBehaviour
 {
